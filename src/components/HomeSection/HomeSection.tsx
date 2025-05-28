216e274fb902fd8bfcd56f7b9a0af1dee1d69848
Uma Sthothrabhashyam <ustotra@gmail.com>
2025-05-28 15:03:52 -0700

    adjusted Projects

--- src/components/HomeSection/HomeSection.tsx
+++ src/components/HomeSection/HomeSection.tsx
@@ -4,21 +4,23 @@ import profilePic from "../../assets/uma_pic.png";
 const HomeSection = () => {
   return (
     <section id="home" className="home">
-      <div className="home-left">
-        <h1>
-          Hi, I'm <span className="highlight">Uma</span>
-        </h1>
-        <h2 className="gradient-role"> Full Stack Developer</h2>
-        <p>
-          I build fast, accessible, and beautiful web interfaces using React,
-          TypesScript and CSS
-        </p>
-        <a href="#contact" className="cta-button">
-          Contact
-        </a>
-      </div>
-      <div className="home-right">
-        <img src={profilePic} className="cutout-img" alt="Uma" />
+      <div className="home-wrapper">
+        <div className="home-left">
+          <h1>
+            Hi, I'm <span className="highlight">Uma</span>
+          </h1>
+          <h2 className="gradient-role"> Full Stack Developer</h2>
+          <p>
+            I build fast, accessible, and beautiful web interfaces using React,
+            TypesScript and CSS
+          </p>
+          <a href="#contact" className="cta-button">
+            Contact
+          </a>
+        </div>
+        <div className="home-right">
+          <img src={profilePic} className="cutout-img" alt="Uma" />
+        </div>
       </div>
     </section>
   );
